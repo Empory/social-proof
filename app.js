be1f7a6d66ab43a7ba61af85fe6cb514d28a3d34
Empory <51644689+Empory@users.noreply.github.com>
2023-01-02 17:50:43 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+//const h1 = document.getElementsByTagName("h1")
+const h1 = document.getElementById("title")
+
+title.setAttribute("style","color: blue; background-color: yellow")
+title.setAttribute("title","test baslik")
+title.setAttribute("class","adana")
+/*
+title.style.color ="blue"
+title.style.backgroundColor = "yellow"
+title.title = "test baslik"
+title.className  = "adana"
+
+/*
+let len = h1.length
+for (let index = 0; index< len; index++) {
+  console.log(h1[index])
+}
+
+
+[...h1].forEach(item=>{
+  console.log(item)
+})
+*/
+
+console.log(h1)
+
+title.innerHTML =""
